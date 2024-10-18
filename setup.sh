3181d95353134e2d7291e3e6b94baa5901f64e57
teaghan <obriaintb@gmail.com>
2024-10-17 20:00:16 -0700

    heroku

--- setup.sh
+++ setup.sh
@@ -3,12 +3,19 @@
 # Ensure the .streamlit directory exists
 mkdir -p ~/.streamlit/
 
-# Append the necessary server settings for Heroku to the existing config.toml
-cat <<EOL >> ~/.streamlit/config.toml
+# Use sed to update or add the necessary server configurations for Heroku
+if grep -q "\[server\]" ~/.streamlit/config.toml; then
+    # Modify the existing [server] section
+    sed -i '' 's/^port.*/port = $PORT/' ~/.streamlit/config.toml
+    sed -i '' 's/^headless.*/headless = true/' ~/.streamlit/config.toml
+    sed -i '' 's/^enableCORS.*/enableCORS = false/' ~/.streamlit/config.toml
+else
+    # Append the [server] section if it doesn't exist (just in case)
+    cat <<EOL >> ~/.streamlit/config.toml
 
-# Heroku-specific server settings
 [server]
 headless = true
 enableCORS = false
 port = \$PORT
 EOL
+fi
