11d7a569178dd8cd78e9d186e1b8ec26e87ea58f
teaghan <obriaintb@gmail.com>
2024-10-20 10:43:28 -0700

    reload data whenever cache is reset

--- utils/session.py
+++ utils/session.py
@@ -4,20 +4,20 @@ from utils.user_data import read_users
 from utils.cookies import cookies_to_session
 
 def load_data(force_reload=True):
-    if ("df_tutors" not in st.session_state) or force_reload:
-        # Load tutor data
-        st.session_state["ai_tutors_data_fn"] = 'ai-tutors/tutor_info.csv'
-        st.session_state["df_tutors"] = read_csv(st.session_state["ai_tutors_data_fn"])
+    #if ("df_tutors" not in st.session_state) or force_reload:
+    # Load tutor data
+    st.session_state["ai_tutors_data_fn"] = 'ai-tutors/tutor_info.csv'
+    st.session_state["df_tutors"] = read_csv(st.session_state["ai_tutors_data_fn"])
 
-    if ("users_config" not in st.session_state) or force_reload:
-        # Load user data
-        st.session_state["users_data_fn"] = 'ai-tutors/users.yaml'#'data/users.yaml'
-        st.session_state["users_config"], st.session_state["authenticator"] = read_users(st.session_state["users_data_fn"])
+    #if ("users_config" not in st.session_state) or force_reload:
+    # Load user data
+    st.session_state["users_data_fn"] = 'ai-tutors/users.yaml'#'data/users.yaml'
+    st.session_state["users_config"], st.session_state["authenticator"] = read_users(st.session_state["users_data_fn"])
 
-    if ("df_access_codes" not in st.session_state) or force_reload:
-        # Load access codes data
-        st.session_state["access_codes_data_fn"] = 'ai-tutors/access_codes.csv'#'data/access_codes.csv'
-        st.session_state["df_access_codes"] = read_csv(st.session_state["access_codes_data_fn"])
+    #if ("df_access_codes" not in st.session_state) or force_reload:
+    # Load access codes data
+    st.session_state["access_codes_data_fn"] = 'ai-tutors/access_codes.csv'#'data/access_codes.csv'
+    st.session_state["df_access_codes"] = read_csv(st.session_state["access_codes_data_fn"])
 
 def user_reset():
     st.session_state.authentication_status = False
