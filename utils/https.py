68252e169b9e0bd4e5c11f3300b10e481092dd39
teaghan <obriaintb@gmail.com>
2025-01-03 07:20:14 -0800

    force ssl

--- utils/https.py
+++ utils/https.py
@@ -7,9 +7,10 @@ def enforce_https():
     # Check the `X-Forwarded-Proto` header set by Heroku
     forwarded_proto = os.environ.get("X_FORWARDED_PROTO", "http")
     if forwarded_proto != "https":
-        st.write(
-            f"""
-            <meta http-equiv="refresh" content="0; url=https://{os.environ['HOST_NAME']}" />
-            """,
-            unsafe_allow_html=True,
-        )
+        st.write(forwarded_proto)
+        #st.write(
+        #    f"""
+        #    <meta http-equiv="refresh" content="0; url=https://{os.environ['HOST_NAME']}" />
+        #    """,
+        #    unsafe_allow_html=True,
+        #)
