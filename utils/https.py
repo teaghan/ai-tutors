acf29d74c82e3570775f02ee1457cff1882987a2
teaghan <obriaintb@gmail.com>
2025-01-03 07:34:28 -0800

    force ssl

--- utils/https.py
+++ utils/https.py
@@ -1,16 +1,15 @@
 
-import os
 import streamlit as st
 
 def enforce_https():
-    """Redirect HTTP requests to HTTPS."""
-    # Check the `X-Forwarded-Proto` header set by Heroku
-    forwarded_proto = os.environ.get("X_FORWARDED_PROTO", "http")
-    if forwarded_proto != "https":
-        st.write(forwarded_proto)
-        #st.write(
-        #    f"""
-        #    <meta http-equiv="refresh" content="0; url=https://{os.environ['HOST_NAME']}" />
-        #    """,
-        #    unsafe_allow_html=True,
-        #)
+    """Redirect HTTP requests to HTTPS using JavaScript."""
+    st.markdown(
+        """
+        <script>
+        if (window.location.protocol !== 'https:') {
+            window.location.href = window.location.href.replace('http://', 'https://');
+        }
+        </script>
+        """,
+        unsafe_allow_html=True,
+    )
